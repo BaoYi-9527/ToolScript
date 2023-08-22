d7112c92b6502669b4babb4568d9a036cbabbc62
weicheng <1648186705@qq.com>
2023-08-22 23:09:58 +0800

    feat:migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('username')->unique()->comment("用户名");
+            $table->string('password')->comment("密码");
+            $table->tinyInteger('gender')->default(0)->comment("性别 0-未知 1-男 2-女");
+            $table->tinyInteger('status')->default(1)->comment('1=可用, 0=不可用');
+            $table->string('nickname')->nullable()->comment("用户昵称");
+            $table->string('avatar')->nullable()->comment("用户头像");
+            $table->integer('phone')->unsigned()->unique()->comment("手机号");
+            $table->string('email')->unique()->comment("电子邮箱");
+            $table->timestamp('email_verified_at')->nullable()->comment("邮箱验证时间");
+            $table->rememberToken()->comment("用户token");
             $table->timestamps();
         });
     }
